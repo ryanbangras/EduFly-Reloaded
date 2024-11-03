98f5b86aa23dd11b11c63c48c7e8d5a2302817f0
jamie <jamie.tan.2023@scis.smu.edu.sg>
2024-11-03 23:47:29 +0800

    adjusted the navbar and code such that the user's name shows up in the navbar and everything is dynamically based on user's login email

--- frontend/Login Page/login.js
+++ frontend/Login Page/login.js
@@ -1,47 +1,72 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
-  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkep4kR1KlxAHUQM8-dJo-b0cOjUA1tCc",
+  authDomain: "edufly-61bfe.firebaseapp.com",
+  projectId: "edufly-61bfe",
+  storageBucket: "edufly-61bfe.firebasestorage.app",
+  messagingSenderId: "467191151194",
+  appId: "1:467191151194:web:cac30fd47ebff5a7233663",
+  measurementId: "G-NQN411353D"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBkep4kR1KlxAHUQM8-dJo-b0cOjUA1tCc",
-    authDomain: "edufly-61bfe.firebaseapp.com",
-    projectId: "edufly-61bfe",
-    storageBucket: "edufly-61bfe.firebasestorage.app",
-    messagingSenderId: "467191151194",
-    appId: "1:467191151194:web:cac30fd47ebff5a7233663",
-    measurementId: "G-NQN411353D"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// when user manually keys in 
 
-  const submit = document.getElementById('submit')
-  submit.addEventListener("click", function(event){
-    event.preventDefault()
+const submit = document.getElementById('submit')
+submit.addEventListener("click", function (event) {
+  event.preventDefault()
 
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            alert("Logging In")
-            window.location.href = "../Homepage/home_teacher.html"
-            // ...
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage)
-            // ..
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("Logging In")
+      window.location.href = "../Homepage/home_teacher.html"
+      // ...
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
+      // ..
     });
 
-  })
+})
+
+// when user uses Google Sign-in method
+const auth = getAuth(app);
+auth.languageCode = "en"
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("google-login-btn")
+googleLogin.addEventListener("click", function () {
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+      window.location.href="../Homepage/home_teacher.html"
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+
+})
