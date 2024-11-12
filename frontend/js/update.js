6ebec77f64a37b8bdd5dbeb64dd5b49c6f7f2401
jamie <jamie.tan.2023@scis.smu.edu.sg>
2024-11-12 20:35:51 +0800

    editing margins of buttons

--- frontend/js/update.js
+++ frontend/js/update.js
@@ -7,7 +7,7 @@ onAuthStateChanged(auth, (user) => {
         // User is signed in, set profile link text
         const profileLink = document.getElementById('profileLink');
         if (profileLink) {
-            profileLink.textContent = `${user.displayName || "Your"} Profile`;
+            profileLink.textContent =  `&#128100;` `Profile`;
         }
     } else {
         // User is not signed in; ensure profile link redirects to login
@@ -28,52 +28,6 @@ document.getElementById("logout-btn").addEventListener("click", () => {
     });
 });
 
-// Redirect to login if user is not authenticated
-// onAuthStateChanged(auth, (user) => {
-//     if (!user) {
-//         // If the user is not logged in, redirect to login page
-//         window.location.href = "../../Login_Page/login.html";
-//     } else {
-//         // If user is logged in, display user's profile name on profile link
-//         const profileLink = document.getElementById('profileLink');
-//         if (profileLink) {
-//             profileLink.textContent = `${user.displayName || "Your"} Profile`;
-//         }
-//     }
-// });
-
-export {onAuthStateChanged as Profile}
-
-// // Logout functionality
-// const logoutBtn = document.getElementById("logout-btn");
-// if (logoutBtn) {
-//     logoutBtn.addEventListener("click", () => {
-//         signOut(auth)
-//             .then(() => {
-//                 alert("Logged out successfully");
-//                 window.location.href = "../Login Page/login_teacher.html";
-//             })
-//             .catch((error) => {
-//                 alert("Error logging out: " + error.message);
-//             });
-//     });
-// }
-
-// linking user to account type
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//         const email = user.email;
-//         if (email == "ryxnng02@gmail.com"){
-//             document.getElementById("account_type").innerText = "Student Account";
-//         }
-//         if (email == "jamietan888@gmail.com"){
-//             document.getElementById("account_type").innerText = "Teacher Account";
-//         }
-//         if (email == "jamie.tan.2023@scis.smu.edu.sg"){
-//             document.getElementById("account_type").innerText = "Parent Account";
-//         }
-//     }
-// });
 
 
 
