4ddc8cd7ab0d1103d30703510a443de34ccf81ee
jamie <jamie.tan.2023@scis.smu.edu.sg>
2024-11-13 22:14:31 +0800

    vercel backend connection

--- backend/app.js
+++ backend/app.js
@@ -6,18 +6,22 @@ const homeworkRoutes = require('./routes/homeworkRoutes');
 const medicalCertificateRoutes = require('./routes/medicalCertificateRoutes');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 async function startServer() {
     try {
         // Connect to both databases
         const homeworkConnection = await connectHomeworkDB();
         const medicalCertificateConnection = await connectMedicalCertificateDB();
+        const options = [ cors({ origin: '*', methods: '*', allowedHeaders: ['Content-Type', 'Authorization'], credentials: true, }) ];
+        app.use(options);
 
         // Pass connections to routes if needed
         app.use('/api', homeworkRoutes(homeworkConnection));
         app.use('/api', medicalCertificateRoutes(medicalCertificateConnection));
+        app.get('/', (req, res) => { res.send('Hello World') })
+        // test to see if connection is successful 
 
         // Start the server
         const PORT = 3000;
